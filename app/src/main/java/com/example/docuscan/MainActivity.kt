d56283e5b3b51a7ad2f7db4e3963b4731c82eb10
Sandra <kuriasandra651@gmail.com>
2024-11-04 00:52:03 +0300

    Column layout for displaying images and scan button

--- app/src/main/java/com/example/docuscan/MainActivity.kt
+++ app/src/main/java/com/example/docuscan/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.docuscan
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,9 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.Uri
+import coil3.compose.AsyncImage
 import com.example.docuscan.ui.theme.DocuScanTheme
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
@@ -49,14 +57,43 @@ class MainActivity : ComponentActivity() {
                         mutableStateListOf<Uri>()
                     }
                     val scannerLauncher = rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.StartActivityForResult(),
+                        contract = ActivityResultContracts.StartIntentSenderForResult(),
                         onResult = {
 
                         }
                     )
                     Column (
-
-                    ){  }
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        imageuris.forEach{ uri ->
+                            AsyncImage(
+                                model =  uri,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                        Button(onClick = {
+                            scanner.getStartScanIntent(this@MainActivity)
+                                .addOnSuccessListener {
+                                    scannerLauncher.launch(
+                                        IntentSenderRequest.Builder(it).build()
+                                    )
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(
+                                        applicationContext,
+                                        it.message,
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                        }) {
+                            Text(text = "Scan PDF")
+                        }
+                    }
                 }
             }
         }
